7272ae02b4c3ae55dcedd76cc3124ed77aaeb705
unknown <kgirdziunas0@gmail.com>
2024-03-14 14:44:48 +0200

    fix

--- src/Components/Gyventojai.jsx
+++ src/Components/Gyventojai.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import styles from "../Styles/gyventojai.module.css"
+// Gyventojai.jsx
+import React from "react";
 
 function Gyventojai({ data, spalva }) {
-    const filteredData = data.filter(item => item.color === spalva);
-
+  // Filter data based on the specified color
+  const filteredData = data.filter(item => item.color === spalva);
+  
   return (
-    <div className={styles["gyventojai-container"]}>Gyventojai ({spalva})
-    <ul>
+    <div>
+      <h2>Gyventojai ({spalva})</h2>
+      <ul>
         {filteredData.length > 0 ? (
-            filteredData.map(item => (
-                <li key={item.id}>{item.name}</li>
-            ))
+          filteredData.map(item => (
+            <li key={item.id}>
+              <span style={{ color: item.color }}>{item.name}</span>
+            </li>
+          ))
         ) : (
-            data.map(item => (
-                <li key={item.id}>{item.name}</li>
-            ))
-        )
-    }
-    </ul>
+          data.map(item => (
+            <li key={item.id}>
+              <span style={{ color: item.color }}>{item.name}</span>
+            </li>
+          ))
+        )}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Gyventojai
+export default Gyventojai;
